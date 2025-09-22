c8a13a312ed3b54d327cf74ebe92a8b93f5976d8
skalyan04 <123116625+skalyan04@users.noreply.github.com>
2025-09-22 10:17:21 -0600

    Update test_task1.py

--- homework1/tests/test_task1.py
+++ homework1/tests/test_task1.py
@@ -1,1 +1,14 @@
+# test_task1.py
 
+import subprocess
+import sys
+import pathlib
+
+def test_task1_output():
+    # Absolute path to task1.py
+    script_path = pathlib.Path("/home/student/cs4300/homework1/src/task1.py")
+
+    result = subprocess.run([sys.executable, str(script_path)], capture_output=True, text=True)
+    output = result.stdout.strip()
+
+    assert output == "Hello, World!"
