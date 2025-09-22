f3f8175a26b76a35df59053f7110e319b3fbbe20
skalyan04 <123116625+skalyan04@users.noreply.github.com>
2025-09-22 10:19:16 -0600

    Update task2.py

--- homework1/src/task2.py
+++ homework1/src/task2.py
@@ -1,1 +1,20 @@
+# task2.py
 
+def get_integer():
+    return 42
+
+def get_float():
+    return 3.14
+
+def get_string():
+    return "Hello, Python"
+
+def get_boolean():
+    return True
+
+
+if __name__ == "__main__":
+    print("Integer:", get_integer())
+    print("Float:", get_float())
+    print("String:", get_string())
+    print("Boolean:", get_boolean())
