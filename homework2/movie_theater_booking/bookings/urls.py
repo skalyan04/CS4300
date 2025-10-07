b8bcae59191a34b81290521fe56ecb9abcf30931
skalyan04 <skalyan@uccs.edu>
2025-10-06 18:01:32 -0600

    Slowly Adding More Code

---
+++ homework2/movie_theater_booking/bookings/urls.py
@@ -0,0 +1,19 @@
+# bookings/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MovieViewSet, SeatViewSet, BookingViewSet, movie_list, book_seat, booking_history
+
+router = DefaultRouter()
+router.register(r'api/movies', MovieViewSet)
+router.register(r'api/seats', SeatViewSet)
+router.register(r'api/bookings', BookingViewSet)
+
+urlpatterns = [
+    # API routes
+    path('', include(router.urls)),
+
+    # Template views
+    path('movies/', movie_list, name='movie_list'),
+    path('movies/<int:movie_id>/book/', book_seat, name='book_seat'),
+    path('booking-history/', booking_history, name='booking_history'),
+]
