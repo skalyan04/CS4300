33feb79b21298f4f0b086961e45af2c0fd1fd3d6
skalyan04 <skalyan@uccs.edu>
2025-10-07 13:55:38 -0600

    Starting and Setting Up PortfolAI Web Landing Page

--- portfolai/home/views.py
+++ portfolai/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here (- SK).
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'home/index.html')
